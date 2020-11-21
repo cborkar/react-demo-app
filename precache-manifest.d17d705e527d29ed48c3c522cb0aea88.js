4fd007ec6bfea342562906becafa6669b6f650c7
cborkar <cborkar49@gmail.com>
2020-11-21 15:50:57 +0530

    Updates

---
+++ precache-manifest.d17d705e527d29ed48c3c522cb0aea88.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c7726583cf84f47dba6f14b6e878214",
+    "url": "/react-demo-app/index.html"
+  },
+  {
+    "revision": "34c71ede23620b41378a",
+    "url": "/react-demo-app/static/css/4.d1806bfc.chunk.css"
+  },
+  {
+    "revision": "01c1d606085d5219e1d3",
+    "url": "/react-demo-app/static/css/6.b24c0b6a.chunk.css"
+  },
+  {
+    "revision": "ecc75f2e7a54183a622e",
+    "url": "/react-demo-app/static/css/7.d5b14116.chunk.css"
+  },
+  {
+    "revision": "9a27fd689ed239cab23e",
+    "url": "/react-demo-app/static/css/8.ded1dfc1.chunk.css"
+  },
+  {
+    "revision": "4ed4b16538962473616e",
+    "url": "/react-demo-app/static/css/main.23348f40.chunk.css"
+  },
+  {
+    "revision": "7174efbcd58885ca720f",
+    "url": "/react-demo-app/static/js/0.b496250e.chunk.js"
+  },
+  {
+    "revision": "0c28bc45316feff60410",
+    "url": "/react-demo-app/static/js/1.e3065835.chunk.js"
+  },
+  {
+    "revision": "34c71ede23620b41378a",
+    "url": "/react-demo-app/static/js/4.12e654b3.chunk.js"
+  },
+  {
+    "revision": "45e7f2c6a1e7c7e23825d3b8f98bac22",
+    "url": "/react-demo-app/static/js/4.12e654b3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "754bf4c46dd52dfe975b",
+    "url": "/react-demo-app/static/js/5.18df6801.chunk.js"
+  },
+  {
+    "revision": "5cfd56103367ebc6a4079b58d5296bfd",
+    "url": "/react-demo-app/static/js/5.18df6801.chunk.js.LICENSE"
+  },
+  {
+    "revision": "01c1d606085d5219e1d3",
+    "url": "/react-demo-app/static/js/6.b3f9bc20.chunk.js"
+  },
+  {
+    "revision": "ecc75f2e7a54183a622e",
+    "url": "/react-demo-app/static/js/7.a69f70b0.chunk.js"
+  },
+  {
+    "revision": "9a27fd689ed239cab23e",
+    "url": "/react-demo-app/static/js/8.fe43185e.chunk.js"
+  },
+  {
+    "revision": "4ed4b16538962473616e",
+    "url": "/react-demo-app/static/js/main.17087671.chunk.js"
+  },
+  {
+    "revision": "b0dad85ef7323aeaa49f",
+    "url": "/react-demo-app/static/js/runtime-main.9dac1105.js"
+  },
+  {
+    "revision": "03ea7ef3cebb9a4bc7aaa0676168693d",
+    "url": "/react-demo-app/static/media/hobbies.03ea7ef3.eot"
+  },
+  {
+    "revision": "29ae1b9c0d10712f6e6cf60808a0a03d",
+    "url": "/react-demo-app/static/media/hobbies.29ae1b9c.woff"
+  },
+  {
+    "revision": "9f45801fc3026513ee53d064359d9b61",
+    "url": "/react-demo-app/static/media/hobbies.9f45801f.ttf"
+  },
+  {
+    "revision": "34fdc4136a7e7e129a6e1897b4c50ee1",
+    "url": "/react-demo-app/static/media/icomoon.34fdc413.ttf"
+  },
+  {
+    "revision": "ad579ff1278c3d4388d15fc27b40b67e",
+    "url": "/react-demo-app/static/media/icomoon.ad579ff1.eot"
+  },
+  {
+    "revision": "c068d37f3b072da2ecefcf5369f219ad",
+    "url": "/react-demo-app/static/media/icomoon.c068d37f.woff"
+  }
+]);
